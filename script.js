83391a8045b40ca9b008990930611ab86355d9c4
deanOcoin <77565607+deanOcoin@users.noreply.github.com>
2021-01-27 14:27:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded',function(event){
-    var dataText = [ "TimerCoin"];
+    var dataText = ["TimerCoin"];
+
+    
     function typeWriter(text, i, fnCallback) {
       if (i < (text.length)) {
        document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
   
         setTimeout(function() {
           typeWriter(text, i + 1, fnCallback)
-        }, 180);
+        }, 365);
       }
       else if (typeof fnCallback == 'function') {
         setTimeout(fnCallback, 1000);
